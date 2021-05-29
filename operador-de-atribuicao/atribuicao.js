047dd960439d75194510fb7a8e5e1fcdf6060dee
João Henrique <joaohenriquecfal@outlook.com>
2021-05-29 11:58:26 -0300

    Operador de atribuição 29/05/21

---
+++ operador-de-atribuicao/atribuicao.js
@@ -0,0 +1,15 @@
+/*
+    Operadores de atribuição
+
+    podem funcionar como formas abreviadas
+*/
+
+    let a = 5;
+    let b = 10;
+    a += b;//  a = a + b (15)
+    a -= b;// a = a - b(-5)
+    a *= b;// a = a * b (50)
+    a /= b;// a = a / b (0.5)
+    a %= b;// a = a % b (0)
+    a **= b;// a = a ** b (9765625)
+    
