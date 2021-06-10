cc0e818bd387a79a3269a0827e6359864dc765d2
João Henrique <joaohenriquecfal@outlook.com>
2021-06-10 09:54:45 -0300

    Obj - Native,Host e User

---
+++ Obj - Native Host e User/Exercicios.js
@@ -0,0 +1,18 @@
+// Liste 5 objetos nativos
+    Function
+    Object
+    Array 
+    String
+    Number
+
+// Liste 5 objetos do browser
+    window
+    document
+    HTMLcollection
+    NodeList
+    element
+
+// Liste 2 Métodos, Propriedades ou Objetos
+// presentes no Chrome que não existem no Firefox
+
+    //...
---
+++ Obj - Native Host e User/index.js
@@ -0,0 +1,42 @@
+/*
+    Native
+
+    Objetos nativos são aqueles definidos na especificação da linguagem e são 
+    implementados independente do host 
+
+    Contrutores de objetos nativos
+
+    Object 
+    String 
+    Array
+    Function
+
+    (Objetos espeficicados pela lingugem.)
+
+ --------------------------------------------------------------------
+
+    Host
+
+   Objetos do host são aqueles implementados pelo próprio ambiente. 
+   Por exemplo no browser possuímos objetos do DOM, como DomList, 
+   HTMLCollection e outros. Em Node.js os objetos do Host são diferentes,
+    já que não estamos em um ambiente do browser.
+
+    // Objetos do browser
+    NodeList
+    HTMLCollection
+    Element
+
+
+    (Objetos espeficicados pelo ambiente.)
+
+     --------------------------------------------------------------------
+
+     User 
+     
+     São objetos criados pelo próprio usuário. Ou seja quando você cria por exemplo 
+     um objeto chado pessoa e coloca nele valores como nome, sobrenome, etc. Também é considerado 
+     Objetos User quando importamos qualquer tipo de biblioteca. 
+
+     (Objetos espeficicados pelo usuário.)
+*/
