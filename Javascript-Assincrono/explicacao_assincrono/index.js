c2543775668aa15ee29305bb30d702a196fd5538
João Henrique <joaohenriquecfal@outlook.com>
2021-07-16 14:30:39 -0300

    Assincrono edit 16/07/2021

--- Javascript-Assincrono/explicacao_assincrono/index.js
+++ Javascript-Assincrono/explicacao_assincrono/index.js
@@ -6,7 +6,9 @@
     Espera uma tarefa acabar para começar a outra
     (Passa por uma linha de cada vez)
 
+
     =>----------------------------------------
+
 
     Assíncrono
 
@@ -16,5 +18,20 @@
     Exemplos(Assíncronos): setTimeout, Ajax, Promises, fetch e Async. 
 
 
+    =>----------------------------------------
+
+
+    VANTAGENS
+
+        Carregamento no Fundo:
+            Não trava o script. É possivel utilizar o site enquanto o precessamento é realizado no fundo.
+
+
+        Função ao Término
+            Podemos ficar de olho no carregamento e executar uma função assim que ele terminar.
+
+
+        Requisição ao Servidor
+            Não precisamos recarregar a página por completo à cada requislição feita ao servidor.
 */
 
