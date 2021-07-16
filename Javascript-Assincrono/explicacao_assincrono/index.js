5a2a44c49eadb39c6f153bdd94ba0e393bdc9ae0
João Henrique <joaohenriquecfal@outlook.com>
2021-07-16 14:16:35 -0300

    Javascript assícrono (Explicação)  16/07/2021

---
+++ Javascript-Assincrono/explicacao_assincrono/index.js
@@ -0,0 +1,20 @@
+ // SÍNCRONO vs ASSÍNCRONO
+
+/*
+    Síncrono
+
+    Espera uma tarefa acabar para começar a outra
+    (Passa por uma linha de cada vez)
+
+    =>----------------------------------------
+
+    Assíncrono
+
+    Não espera uma tarefa acabar para começar outra. Ele vai executando as outras
+    até receber as informações necessárias.
+
+    Exemplos(Assíncronos): setTimeout, Ajax, Promises, fetch e Async. 
+
+
+*/
+
