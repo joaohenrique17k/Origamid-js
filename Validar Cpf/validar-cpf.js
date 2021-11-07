bba6100dcd82eb242d9b901a218617ef1ddcdf3f
João Henrique <joaohenriquecfal@outlook.com>
2021-11-06 21:17:56 -0300

    Ajustes

--- Validar Cpf/validar-cpf.js
+++ Validar Cpf/validar-cpf.js
@@ -14,10 +14,10 @@ export default class ValidarCpf {
   }
   validar(cpf) {
     const matchCpf = cpf.match(/(?:\d{3}[-.\s]?){3}\d{2}/g);
-    return (matchCpf && matchCpf[0] === cpf);
+    return matchCpf && matchCpf[0] === cpf;
   }
   validarNaMudanca(cpfElement) {
-    if(this.validar(cpfElement.value)) {
+    if (this.validar(cpfElement.value)) {
       cpfElement.value = this.formatar(cpfElement.value);
       cpfElement.classList.add('valido');
       cpfElement.classList.remove('erro');
@@ -31,13 +31,16 @@ export default class ValidarCpf {
   adicionarEvento() {
     this.element.addEventListener('change', () => {
       this.validarNaMudanca(this.element);
-    })
+    });
   }
   adicionarErroSpan() {
     const erroElement = document.createElement('span');
     erroElement.classList.add('erro-text');
     erroElement.innerText = 'CPF Inválido';
-    this.element.parentElement.insertBefore(erroElement, this.element.nextElementSibling);
+    this.element.parentElement.insertBefore(
+      erroElement,
+      this.element.nextElementSibling,
+    );
   }
   iniciar() {
     this.adicionarEvento();
